7dae6ee7b775b83ac8c657aaf18afc5f2c665c11
Kurt Sanders <kurt@kurtsanders.com>
2026-01-21 08:32:11 -0500

    Update BatteryLevelWatcher.groovy

--- resources/apps/Battery Level Watcher/BatteryLevelWatcher.groovy
+++ resources/apps/Battery Level Watcher/BatteryLevelWatcher.groovy
@@ -6,6 +6,7 @@ import groovy.transform.Field
 @Field static String NAMESPACE                     = "kurtsanders"
 @Field static String AUTHOR_NAME                   = "Kurt Sanders"
 @Field static final String VERSION                 = "1.0.0"
+@Field static final String defaultDateTimeFormat 	= 'MMM d, yyyy, h:mm a'
 
 definition(
     name              : APP_NAME,
@@ -43,51 +44,57 @@ def pageStatus() {
                 def sectionTitile
                 switch(i) {
                     case 0:
-                    	sectionTitle = getFormat('header-red',"Batteries with errors or no status")
+                    	sectionTitle = getFormat('header-red',"Batteries with Errors or No Status")
                     break
                     case 1:
-                    	sectionTitle = getFormat('header-red',"Batteries with low charge (less than $settings.level1)")
+                    	sectionTitle = getFormat('header-red',"Batteries with LOW charge (less than $settings.level1)%")
                     break
                     case 2:
-                    	sectionTitle = getFormat('header-orange',"Batteries with medium charge (between $settings.level1 and $settings.level3)")
+                    	sectionTitle = getFormat('header-blue',"Batteries with Medium Charge (between ${settings.level1}% and ${settings.level3}%)")
                     break
                     case 3:
-                    	sectionTitle = getFormat('header-orange',"Batteries with high charge (more than $settings.level3)")
+                    sectionTitle = getFormat('header-red',"Batteries with High Charge (more than ${settings.level3}%)")
                     break
                     case 4:
-                    	sectionTitle = getFormat('header-green',"Batteries with full charge")
+                    	sectionTitle = getFormat('header-green',"Batteries with Full Charge")
                     break
                 }                    
-                section("${sectionTitle}") {
+                section() {
+                    logDebug "tempList => ${tempList}"
                     tempList.sort { it.battery }.each { userMap ->
                         lineout += '<tr>' + 
                             "<td>${userMap.battery}%</td>" +
                             "<td><a href=http://${location.hub.localIP}/device/edit/${userMap.id}>${userMap?.name}</a></td>" +
                             "<td>${userMap.type}</td>" +
+                            "<td>${userMap.lastBatteryReportDuration}</td>" +
+                            "<td>${userMap?.lastActivity}</td>" +
                             '</tr>'
                     }
                 def htmlStyle 	= '<html><head><style>' + 
-                    			  'table,th,td{border: 1px solid black;border-collapse:collapse;padding:8px;text-align: center;}' +
-                    			  'table{margin-left:auto;margin-right:auto;width: 50%;}'+
+								  'table th, td {text-align: center;padding: 8px;}' +
+								  'tr:nth-child(even) {background-color: #D6EEEE;}' +                    
+                    			  'table {border-collapse: collapse;width: 100%;}' +
                     			  '</style></head>'
-                def htmlHead 	= '<table><thead><tr>' + 
-                    '<th>Battery Level</th>' +
+                def htmlHead 	= "<table><caption>${sectionTitle}</caption><thead>" + 
+                    '<tr><th>Battery Level</th>' +
                     '<th>Device Name</th>' +
                     '<th>Device Type</th>' +
+                    '<th>Last Battery Report Duration</th>' +
+                    '<th>Last Activity</th>' +
                     '</thead></tr><tbody>'
                 def htmlFoot 	= '</table></tbody></html>'
                     paragraph "${htmlStyle}${htmlHead}${lineout}${htmlFoot}"
                 }
             }
         }
-        section(sectionHeader("Setup")) {
+        section(sectionHeader("Application Setup")) {
             href "pageStatus", title:"Refresh", description:""
-            href "pageConfigure", title:"Configure", description:""
+            href "pageConfigure", title:"Application Configure", description:""
             displayPaypal()
         }
         section(sectionHeader("Logging Options")) {
             if (logLevel == null && logLevelTime == null) {
-                logInfo "${app.name}: Setting Innital logLevel and LogLevelTime defaults"
+                logInfo "${app.name}: Setting Inital logLevel and logLevelTime defaults"
                 app.updateSetting(logLevel, [type: "enum", value: [5]])
                 app.updateSetting(logLevelTime, [type: "enum", value: [30]])
             }
@@ -97,70 +104,33 @@ def pageStatus() {
             input name: "logLevelTime", type: "enum", title: fmtTitle("Logging Level Time"), submitOnChange: true,
                 description: fmtDesc("Time to enable Debug/Trace logging"),defaultValue: 10, options: LOG_TIMES
         }
+        displayPaypal()
     }
 }
 
 // Show Configure Page
 def pageConfigure() {
-    def inputBattery   = [
-        name:           "devices",
-        type:           "capability.battery",
-        title:          "Which devices with batteries?",
-        multiple:       true,
-        required:       true
-    ]
-    def inputLevel1    = [
-        name:           "level1",
-        type:           "number",
-        title:          "Low battery threshold?",
-        defaultValue:   "20",
-        required:       true
-    ]
-    def inputLevel3    = [
-        name:           "level3",
-        type:           "number",
-        title:          "Medium battery threshold?",
-        defaultValue:   "70",
-        required:       true
-    ]
-    def inputTime      = [
-        name:           "time",
-        type:           "time",
-        title:          "Notify at what time daily?",
-        required:       true
-    ]
-    def inputPush      = [
-        name:           "pushMessage",
-        type:           "bool",
-        title:          "Send push notifications?",
-        defaultValue:   true
-    ]
-    def inputPushBattery= [
-        name:           "pushMessageBattery",
-        type:           "bool",
-        title:          "Send push notifications for all devices?",
-        defaultValue:   false
-    ]
     def pageProperties = [
         name:           "pageConfigure",
-        title:           getFormat("title","${APP_NAME} Configuration"),
+        title:           getFormat("title","${APP_NAME} Application Configuration"),
         nextPage:       "pageStatus",
         uninstall:      true
     ]
     return dynamicPage(pageProperties) {
-        section(getFormat('header-green',"Select Devices")) {
-            input inputBattery
+        section(getFormat('header-green',"Select Devices with a Battery attribute")) {
+            input name: "devices", 				type: "capability.battery", title: "Which devices with batteries?", multiple: true, required: true
         }        
-        section(getFormat('header-green',"Settings")) {
-            input inputLevel1
-            input inputLevel3
+        section(getFormat('header-green',"Application Settings")) {
+            input name: "level1", 				type: "number", title: "Low battery threshold?", defaultValue: "20", required: true
+            input name: "level3",				type: "number", title: "Medium battery threshold?", defaultValue: "70", required: true
         }        
         section(getFormat('header-green',"Low Battery Notification")) {
-            input inputTime
-            input inputPush
-            input inputPushBattery
-            input "sendPushMessage", "capability.notification", title: "Select Notification Devices", multiple: true, required: false
-            input name: "modes", type: "mode", title: "Only send notifications if mode is", multiple: true, required: false
+            input name: "time", 				type: "time", title: "Notify at what time daily?", required: true
+            input name: "pushMessage", 			type: "bool", title: "Send push notifications?", defaultValue: true
+            input name: "sendPushMessage", 		type: "capability.notification", title: "Select Notification Devices", 	multiple: true, required: false
+            input name: "includeHubName", 		type: "bool", title: "Include hub name in notifications (${location.name})", defaultValue: true
+            input name: "pushMessageBattery", 	type: "bool", title: "Send push notifications for all devices?", 	defaultValue: false
+            input name: "modes", 				type: "mode", title: "Only send notifications if mode is", multiple: true, required: false
         }
         section(getFormat('header-green',"Options")) {
             label title:"Assign a name", required:false
@@ -204,13 +174,15 @@ def updateStatus() {
         try {
             switch(it.currentBattery.toInteger()) {
                 case {it > 100}:
-	                send("${it.displayName} battery is ${it.currentBattery}, which is over 100.")
+	                send("${it.displayName} battery is ${it.currentBattery}%, which is over 100.")
     	            break
+                case {it >= settings.level1}:
+                	break
                 case {it < settings.level1}:
-                	send("${it.displayName} battery is ${it.currentBattery} (threshold ${settings.level1}.)")
+                	send("${it.displayName} battery is ${it.currentBattery}% which is lesss than threshold level: ${settings.level1}%.)")
                 	break
                 default:
-	                send("${it.displayName} battery is not reporting.")
+	                send("${it.displayName} battery is not reporting an integer battery level.")
     	            break
         	}
         } catch (e) {
@@ -222,6 +194,7 @@ def updateStatus() {
 }
 
 def makeDeviceList() {
+    updateStatus()
     def deviceList = []
     int group 
     settings.devices.each() {
@@ -230,7 +203,7 @@ def makeDeviceList() {
                 case {it <= settings.level1}:
                 group = 1
                 break
-                case {it <= settings.level2}:
+                case {it <= settings.level3}:
                 group = 2
                 break
                 case {it < 100}:
@@ -248,16 +221,52 @@ def makeDeviceList() {
             logErr "Caught error checking battery status for '${it.displayName}'."
             logErr e
         }
+        def lastBatteryReportDuration
+        def lastActivity = it.getLastActivity()
+        def lastBatteryEvent = it.events(max:200).find{it.name == 'battery'}
         // Create deviceList of devices with group numbers
         def map = [:] // Create an empty map
-        map.group 	= group
-        map.id  	= it.id 
-        map.name	= it.label?:it.displayName
-        map.battery	= it.currentBattery
-        map.type	= it.typeName
+        map.group 						= group
+        map.id  						= it.id 
+        map.name						= it.label?:it.displayName
+        map.battery						= it.currentBattery
+        map.type						= it.typeName
+        map.lastActivity 				= durationCalc(lastActivity)
+        map.lastBatteryReportDuration 	= durationCalc(lastBatteryEvent?.date)
         deviceList << map // Add map to the list
     }
+    logDebug "deviceList = ${deviceList}"
     return deviceList
+}
+
+def durationCalc(endDate) {
+    def durationOutput=''
+    if (endDate) {
+        def today = new Date()
+        use(groovy.time.TimeCategory) {
+            def duration = today - endDate
+            if (duration.days>0) {
+                if (duration.days==1) {
+                    durationOutput += "${duration.days} day, "
+                } else {
+                    durationOutput += "${duration.days} days, "                    
+                }
+            }
+            if (duration.hours>0) {
+                if (duration.hours==1) {
+                    durationOutput += "${duration.hours} hour, "
+                } else {
+                    durationOutput += "${duration.hours} hours, "                    
+                }
+            }
+            if (duration.minutes==1) {
+                durationOutput += duration.minutes + ' min.'
+            } else {
+                durationOutput += duration.minutes + ' mins.'
+            }
+        }
+    }
+    return durationOutput?:'unknown'
 }
 
 Boolean isModeOK() {
@@ -267,7 +276,10 @@ Boolean isModeOK() {
 }
 
 def send(msg) {
-    if (settings.pushMessage && isModeOK()) {     
+    if (settings.pushMessage && isModeOK()) {
+        if (settings["includeHubName"]) {
+            msg = "${location.name}: ${msg}"
+        }
         sendPushMessage.deviceNotification(msg)
     } 
 }
